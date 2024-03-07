8c32590e8d4d279c706573bb087f9d770942cfc9
asadal <blaulaut@gmail.com>
2024-03-07 13:32:38 +0900

    Update chat_kyori.py

--- chat_kyori.py
+++ chat_kyori.py
@@ -5,6 +5,19 @@ from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 # from create_database import generate_data_store
 
+# these three lines swap the stdlib sqlite3 lib with the pysqlite3 package
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
 CHROMA_PATH = "chroma"
 
 PROMPT_TEMPLATE = """
